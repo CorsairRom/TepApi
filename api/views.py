096512320d4031c4cff3d981e37bb1fd4c489457
CorsairRom <ric.romero@duocuc.cl>
2023-07-10 17:40:11 -0400

    add: finish test with postman

--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from api.serializers import SerializerEmpleados, SerializerEmpresas
 from api.models import Empleados, Empresas
 
 from rest_framework import status, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 
@@ -14,3 +15,5 @@ class EmpresasViewSet(viewsets.ModelViewSet):
 class EmpleadosViewSet(viewsets.ModelViewSet):
     serializer_class = SerializerEmpleados
     queryset = Empleados.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['empresa']
